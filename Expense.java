be6afcb05d30fa46ea277536166f2aab04ea158f
codeaisha <aishazafri6@gmail.com>
2025-04-07 15:34:01 +0530

    Create Expense.java

---
+++ Expense.java
@@ -0,0 +1,13 @@
+public class Expense {
+    public String name;
+    public int cost;
+    public int value;
+    public String category;
+
+    public Expense(String name, int cost, int value, String category) {
+        this.name = name;
+        this.cost = cost;
+        this.value = value;
+        this.category = category;
+    }
+}
